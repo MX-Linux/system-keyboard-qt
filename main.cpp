ab5631cb667e4f94040a0a553860d146cf72a3b1
Dolphin Oracle <dolphinoracle@gmail.com>
2020-11-30 20:41:22 -0500

    add translation stuff for strings not covered by xkeyboard-config

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "window.h"
 #include <QApplication>
+#include <QTranslator>
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator qtTran;
+    qtTran.load(QString("qt_") + QLocale::system().name());
+    a.installTranslator(&qtTran);
+
+    QTranslator appTran;
+    appTran.load(QString("system-keyboard-qt_") + QLocale::system().name(), "/usr/share/system-keyboard-qt/locale");
+    a.installTranslator(&appTran);
+
+
     Window w;
     w.show();
 
---
+++ translations/system-keyboard-qt_cs.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>SelectLayoutDialog</name>
+    <message>
+        <location filename="../selectlayoutdialog.ui" line="14"/>
+        <source>Select Keyboard Layout and Variant</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../selectlayoutdialog.ui" line="29"/>
+        <source>Layout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../selectlayoutdialog.ui" line="47"/>
+        <source>Variants</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../selectlayoutdialog.cpp" line="56"/>
+        <source>No Variant</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../window.ui" line="14"/>
+        <source>System Keyboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.ui" line="28"/>
+        <source>Layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.ui" line="34"/>
+        <source>Keyboard Model</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.ui" line="46"/>
+        <source>Keyboard Layouts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.ui" line="133"/>
+        <source>Hotkeys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.ui" line="156"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="143"/>
+        <location filename="../window.cpp" line="161"/>
+        <location filename="../window.cpp" line="182"/>
+        <location filename="../window.cpp" line="195"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="143"/>
+        <location filename="../window.cpp" line="161"/>
+        <location filename="../window.cpp" line="195"/>
+        <source>Failed to open file: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="182"/>
+        <source>Command exited with code non-zero: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="195"/>
+        <source>Most settings will not be loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../window.cpp" line="306"/>
+        <source>Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
