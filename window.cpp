2a7ad5cab6ebadb745f8c79515fa7d48d472550e
TheNiteCoder <coderofthenite@gmail.com>
2020-12-17 20:58:07 -0500

    Changing /etc/default/keyboard reload command from setxkbmap to udevadm

--- window.cpp
+++ window.cpp
@@ -202,25 +202,28 @@ bool Window::apply()
         stream << parser.source();
         io.close();
     }
-    QEventLoop loop;
-    QProcess proc;
-    QObject::connect(&proc, QOverload<int>::of(&QProcess::finished), &loop, &QEventLoop::quit);
-    auto command = QString("setxkbmap");
-    auto commandOptions = QStringList()
-               << "-model" << getModel()
-               << "-layout" << getLayoutsAndVariants().first.join(',')
-               << "-variant" << getLayoutsAndVariants().second.join(',')
-               << "-option" << getOptions().join(',');
-    proc.start("setxkbmap", commandOptions);
-    loop.exec();
-    QObject::disconnect(&proc, nullptr, nullptr, nullptr);
-    if(proc.exitCode() != 0)
-    {
-        QMessageBox::critical(this, tr("Error"), tr("Command exited with code non-zero: ") + (QStringList() << command << commandOptions).join(' '), QMessageBox::Close);
-        qDebug() << proc.readAllStandardError();
-        return false;
-    }
-    proc.close();
+    // makes X see the changes to /etc/default/keyboard right away
+    // is better than setxkbmap because if you logout but don't reboot the changes don't apply
+    system("udevadm trigger --subsystem-match=input --action=change");
+//    QEventLoop loop;
+//    QProcess proc;
+//    QObject::connect(&proc, QOverload<int>::of(&QProcess::finished), &loop, &QEventLoop::quit);
+//    auto command = QString("setxkbmap");
+//    auto commandOptions = QStringList()
+//               << "-model" << getModel()
+//               << "-layout" << getLayoutsAndVariants().first.join(',')
+//               << "-variant" << getLayoutsAndVariants().second.join(',')
+//               << "-option" << getOptions().join(',');
+//    proc.start("setxkbmap", commandOptions);
+//    loop.exec();
+//    QObject::disconnect(&proc, nullptr, nullptr, nullptr);
+//    if(proc.exitCode() != 0)
+//    {
+//        QMessageBox::critical(this, tr("Error"), tr("Command exited with code non-zero: ") + (QStringList() << command << commandOptions).join(' '), QMessageBox::Close);
+//        qDebug() << proc.readAllStandardError();
+//        return false;
+//    }
+//    proc.close();
     return true;
 }
 
