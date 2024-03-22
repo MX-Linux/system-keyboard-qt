6e8f9494fa0f66c031a57ebdd5b15d85fa891b9f
Dolphin Oracle <dolphinoracle@gmail.com>
2024-03-22 18:30:22 -0400

    update translations

--- translations/system-keyboard-qt_ko.ts
+++ translations/system-keyboard-qt_ko.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="sl">
+<TS version="2.1" language="ko">
 <context>
     <name>FilterableComboBox</name>
     <message>
@@ -14,22 +14,22 @@
     <message>
         <location filename="../selectlayoutdialog.ui" line="14"/>
         <source>Select Keyboard Layout and Variant</source>
-        <translation>Izberite razpored in različico tipkovnice</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../selectlayoutdialog.ui" line="29"/>
         <source>Layout</source>
-        <translation>Razporeditev</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../selectlayoutdialog.ui" line="47"/>
         <source>Variants</source>
-        <translation>Izvedbe</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../selectlayoutdialog.cpp" line="57"/>
         <source>No Variant</source>
-        <translation>Brez izvedbe</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -39,64 +39,60 @@
         <location filename="../window.cpp" line="114"/>
         <location filename="../window.cpp" line="118"/>
         <source>System Keyboard</source>
-        <translation>Sistemska tipkovnica</translation>
+        <translation>시스템 키보드</translation>
     </message>
     <message>
         <location filename="../window.ui" line="28"/>
         <source>Layouts</source>
-        <translation>Razporeditve</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../window.ui" line="34"/>
         <source>Keyboard Model</source>
-        <translation>Model tipkovnice</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>search</source>
-        <translation type="vanished">iskanje</translation>
+        <translation type="vanished">검색</translation>
     </message>
     <message>
         <location filename="../window.ui" line="46"/>
         <source>Keyboard Layouts</source>
-        <translation>Razpored tipkovnice</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../window.ui" line="136"/>
         <source>Hotkeys</source>
-        <translation>Hitre tipke</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../window.ui" line="159"/>
         <source>Advanced</source>
-        <translation>Napredno</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../window.ui" line="187"/>
         <source>Help</source>
-        <translation>Pomoč</translation>
+        <translation>도움말</translation>
     </message>
     <message>
         <location filename="../window.cpp" line="187"/>
         <location filename="../window.cpp" line="212"/>
         <location filename="../window.cpp" line="238"/>
         <source>Error</source>
-        <translation>Napaka</translation>
+        <translation>에러</translation>
     </message>
     <message>
         <location filename="../window.cpp" line="187"/>
         <location filename="../window.cpp" line="212"/>
         <location filename="../window.cpp" line="238"/>
         <source>Failed to open file: </source>
-        <translation>Neuspešno odpiranje datoteke:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../window.cpp" line="238"/>
         <source>Most settings will not be loaded</source>
-        <translation>Večina nastavitev ne bo naloženih</translation>
-    </message>
-    <message>
-        <source>Disabled</source>
-        <translation type="vanished">Onemogočeno</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
